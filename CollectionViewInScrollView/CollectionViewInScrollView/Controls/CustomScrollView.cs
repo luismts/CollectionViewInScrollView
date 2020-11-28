bd1ee469aa21c10970dbd0816c528b4d6cae8985
luismts <deivi_0326@hotmail.com>
2020-11-28 00:10:43 -0400

    Now working with references name control

--- CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomScrollView.cs
+++ CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomScrollView.cs
@@ -7,9 +7,19 @@ namespace CollectionViewInScrollView
 {
     public class CustomScrollView : ScrollView
     {
+        private CollectionView _collectionView;
+
+        [TypeConverter(typeof(ReferenceTypeConverter))]
+        public CollectionView CollectionView
+        {
+            set => _collectionView = value;
+        }
+
         protected override void LayoutChildren(double x, double y, double width, double height)
         {
-            (this.Content as StackLayout).Children[2].HeightRequest = height;
+            if (_collectionView != null)
+                _collectionView.HeightRequest = height;
+
             base.LayoutChildren(x, y, width, height);
         }
 
