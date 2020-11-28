a7b3ae1c8d88c88b6b1b2d5063f23828a2189281
luismts <deivi_0326@hotmail.com>
2020-11-27 23:33:05 -0400

    Add project files.

---
+++ CollectionViewInScrollView/CollectionViewInScrollView/ViewModels/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewInScrollView
+{
+    public class ListViewAutoFitContentViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewBookInfo> bookInfo;
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewAutoFitContentViewModel()
+        {
+            GenerateSource();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<ListViewBookInfo> BookInfo
+        {
+            get { return bookInfo; }
+            set { this.bookInfo = value; }
+        }
+
+        #endregion
+
+        #region Generate Source
+
+        private void GenerateSource()
+        {
+            ListViewBookInfoRepository bookInfoRepository = new ListViewBookInfoRepository();
+            bookInfo = bookInfoRepository.GetBookInfo();
+        }
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
