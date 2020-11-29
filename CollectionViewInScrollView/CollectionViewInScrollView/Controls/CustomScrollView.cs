fe99e898fefb15238128bf9deee0c7aad78760b3
luismts <deivi_0326@hotmail.com>
2020-11-29 19:44:08 -0400

    Scroll test

--- CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomScrollView.cs
+++ CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomScrollView.cs
@@ -3,25 +3,86 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
-namespace CollectionViewInScrollView
+namespace CollectionViewInScrollView.Controls
 {
     public class CustomScrollView : ScrollView
     {
-        private CollectionView _collectionView;
+        private ItemsView _scrollableView;
+        private double previousScrollViewPosition = 0;
+        private double previousScrollableViewPosition = 0;
+
+        public CustomScrollView()
+        {
+            this.Scrolled += CustomScrollView_Scrolled;
+        }
 
         [TypeConverter(typeof(ReferenceTypeConverter))]
-        public CollectionView CollectionView
+        public ItemsView ScrollableView
         {
-            set => _collectionView = value;
+            set
+            {
+                _scrollableView = value;
+                _scrollableView.Scrolled += _scrollableView_Scrolled; ;
+            }
         }
 
         protected override void LayoutChildren(double x, double y, double width, double height)
         {
-            if (_collectionView != null)
-                _collectionView.HeightRequest = height;
+            if (_scrollableView != null)
+                _scrollableView.HeightRequest = height;
 
             base.LayoutChildren(x, y, width, height);
         }
+
+        private void CustomScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            double scrollingSpace = this.ContentSize.Height - this.Height;
+
+            if (previousScrollViewPosition < e.ScrollY)
+            {
+                //scrolled down
+                
+            }
+            else if (scrollingSpace <= e.ScrollY)
+            {
+                // Touched bottom view
+            }
+            else
+            {
+                //scrolled up
+
+            }
+
+            previousScrollViewPosition = e.ScrollY;
+        }
+
+        private void _scrollableView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            double scrollingSpace = this.ContentSize.Height - this.Height;
+            
+            if (previousScrollableViewPosition < e.VerticalOffset)
+            {
+                //scrolled down
+                
+            }
+            else if (scrollingSpace <= e.VerticalOffset)
+            {
+                // Touched bottom view
+            }
+            else
+            {
+                //scrolled up
+
+            }
+
+            previousScrollableViewPosition = e.VerticalOffset;
+        }
+
+        
+
+        
+
+        
 
     }
 }
