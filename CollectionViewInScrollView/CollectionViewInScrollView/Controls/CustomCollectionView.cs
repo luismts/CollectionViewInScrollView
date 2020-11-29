45fa7d66fb615d207f88f45b1a63568ef3144f4c
luismts <deivi_0326@hotmail.com>
2020-11-29 19:43:39 -0400

    Template reference removed. No way to access the views inside of it

--- CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomCollectionView.cs
+++ CollectionViewInScrollView/CollectionViewInScrollView/Controls/CustomCollectionView.cs
@@ -8,7 +8,6 @@ namespace CollectionViewInScrollView.Controls
     public class CustomCollectionView : CollectionView
     {
         private ScrollView _scrollView;
-        private Layout<View> _templateReference;
         private int _rowHeigt, _columns = 1;
         private double previousScrollViewPosition = 0;
 
@@ -26,12 +25,6 @@ namespace CollectionViewInScrollView.Controls
                 _scrollView.Scrolled += _scrollView_Scrolled;
             }
         }
-
-        //[TypeConverter(typeof(ReferenceTypeConverter))]
-        //public Layout<View> TemplateContainer
-        //{
-        //    set => _templateReference = value;
-        //}
 
         private int RowCount => Convert.ToInt32(ItemsSource.Cast<object>().ToList().Count);
 
@@ -52,61 +45,7 @@ namespace CollectionViewInScrollView.Controls
             if (_rowHeigt > 0)
             {
                 HeightRequest = (_rowHeigt * rowCount) / _columns;                
-            }
-            else
-            {
-                if (_templateReference is null)
-                    return;
-                
-                double rowHeight = _templateReference switch
-                {
-                    Grid        => (_templateReference as Grid).Height,
-                    StackLayout => (_templateReference as StackLayout).Height,
-                    _           => 0
-                };
-
-                if (rowHeight <= 0)
-                {
-                    rowHeight = _templateReference switch
-                    {
-                        Grid        => (_templateReference as Grid).HeightRequest,
-                        StackLayout => (_templateReference as StackLayout).HeightRequest,
-                        _           => 0
-                    };
-                }
-
-                if (rowHeight > 0)
-                {
-                    double spacing = _templateReference switch
-                    {
-                        Grid => (_templateReference as Grid).RowSpacing,
-                        StackLayout => (_templateReference as StackLayout).Spacing,
-                        _ => 0
-                    };
-
-                    //Thickness padding = _templateReference switch
-                    //{
-                    //    Grid => (_templateReference as Layout).Padding,
-                    //    StackLayout => (_templateReference as Layout).Padding,
-                    //    _ => 0
-                    //};
-
-                    //double verticalPadding = padding.Bottom + padding.Top;
-
-                    Thickness margin = _templateReference switch
-                    {
-                        Grid => (_templateReference as Layout).Margin,
-                        StackLayout => (_templateReference as Layout).Margin,
-                        _ => 0
-                    };
-
-                    double verticalMargin = margin.Bottom + margin.Top;
-
-                    HeightRequest = (rowHeight * rowCount) + (spacing * rowCount) + /*(verticalPadding * rowCount) +*/ (verticalMargin * rowCount);
-                }
-            }
-            
-            
+            } 
         }
 
         protected override void OnBindingContextChanged()
